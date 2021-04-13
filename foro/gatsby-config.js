ee19da6db6acdbca6844d54b2cd490fe697c47ec
Carlos <carlosvilla@google.com>
2021-04-13 16:38:30 -0500

    Inicial setup

---
+++ foro/gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Foro de noticias CUES`,
+    author: `Cues News`,
+    description: `My site description...`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/gatsbyjs`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/gatsbyjs`,
+      },
+    ],
+  },
+}
---
+++ foro/src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default function Bio() {
+  return (
+    <>
+      Cues noticias por <Styled.a href="http://example.com/">El Inge</Styled.a>{` `}
+      .
+    </>
+  )
+}
